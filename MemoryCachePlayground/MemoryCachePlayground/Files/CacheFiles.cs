d78e7bf8b3ccf22536daae1ce9b188b3116024bd
André Davis <cubicle.jockey@live.com>
2018-09-28 12:16:55 -0700

    Checkpoint. Restarting Computer

---
+++ MemoryCachePlayground/MemoryCachePlayground/Files/CacheFiles.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+
+namespace MemoryCachePlayground.Files
+{
+    [TestClass]
+    public class CacheFiles
+    {
+        [TestMethod]
+        public void FileReadNotInCache()
+        {
+            var stopWatch = new Stopwatch();
+
+
+        }
+    }
+}
---
+++ MemoryCachePlayground/MemoryCachePlayground/Files/FileManager.cs
@@ -0,0 +1,45 @@
+﻿using MemoryCachePlayground.Constants;
+using System;
+using System.IO;
+using System.Runtime.Caching;
+
+namespace MemoryCachePlayground.Files
+{
+    public class FileManager
+    {
+        private readonly FileInfo File;
+        private readonly MemoryCache cache;
+
+
+        public FileManager(string file)
+        {
+            if (string.IsNullOrWhiteSpace(file)) { throw new ArgumentNullException(nameof(file)); }
+
+            File = new FileInfo(file);
+            File.Refresh();
+
+            if (!File.Exists) { throw new FileNotFoundException("Check that file exists.", file); }
+
+            cache = MemoryCache.Default;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public (string file, string content) RetrieveFile()
+        {
+            if (cache.Contains(CacheKeys.Story))
+            {
+                return (File.Name, (string)cache.GetCacheItem(CacheKeys.Story)?.Value);
+            }
+
+            string content;
+            using (var streamReader = File.OpenText())
+            {
+                content = streamReader.ReadToEnd();
+            }
+            return (File.Name, content);
+        }
+    }
+}
